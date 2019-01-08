68d4e90493c211e3316dffa9e7f256093e60b154
Simon Uyttenhove <simon.uyttenhove@gmail.com>
2019-01-08 21:45:07 +0100

    added xunit framework.

---
+++ ProcessAnalyzerConsole/Tests/Test_AnalyzeInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ProcessAnalyzerConsole.Tests
+{
+    
+    public class Test_AnalyzeInput
+    {
+        [Fact]
+        public void Test_FirstArgWrong() { }
+
+        [Fact]
+        public void Test_SecondArgWrong() { }
+
+        [Fact]
+        public void Test_ThirdArgWrong() { }
+
+    }
+}
